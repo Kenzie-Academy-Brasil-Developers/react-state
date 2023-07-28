59350242dd420cf19af0b36f0b6c90c5424cb77d
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-28 16:45:14 -0300

    react state example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  )
+  /*
+  const [numberList, setNumberList] = useState([1, 2, 3]);
+
+  const total = numberList.reduce((prevValue, number) => {
+    return prevValue + number;
+  }, 0);
+
+  const addNumber = () => {
+    setNumberList([...numberList, 4]);
+  }
+  */
+  const [teacherList, setTeacherList] = useState([
+    {
+      name: "Alex",
+      job: "Instrutor React",
+      budget: 7000,
+    }
+  ])
+
+  const javascriptTeachers = teacherList.filter(teacher => teacher.job === "Instrutor Javascript");
+
+  console.log("--TEACHER LIST--")
+  console.log(teacherList);
+  console.log("--JAVASCRIPT TEACHERS")
+  console.log(javascriptTeachers);
+
+  const addTeacher = () => {
+    setTeacherList([...teacherList, { name: "Caique", job: "Instrutor Javascript", budget: 7000}])
+  }
+
+   return (
+      <div className="App">
+        <button onClick={addTeacher}>Adicionar professor</button>
+      </div>
+   );
 }
 
-export default App
+export default App;
